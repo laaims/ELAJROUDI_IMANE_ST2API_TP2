96c990d055529ade4c70c8291c67bf9013f9a9cd
imane <imane.el.ajroudi@efrei.net>
2022-06-18 22:42:07 +0100

    first commit

---
+++ ELAJROUDI_IMANE_ST2API_TP2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+public class Program
+
+{
+    
+public partial class WeatherApp
+    {
+        public WeatherApp()
+        {
+            HttpClient= new HttpClient();
+        }
+//public async Task GetWeatherAtCoord(Coordinates coord){
+    //var uri = BuildUri(coord.Lat, cood.lon)
+    //await Connect(uri)}
+        // API key
+        private const string APIKey = "d09d85ca6f9120e8d0c2857a93e05f9b";
+        private HttpClient HttpClient{get;set;}
+
+        // Graphical Interface
+        private void WeatherApp_Load(object sender, EventArgs e)
+        {
+            getWeather();
+        }
+
+         void getWeather()
+        {
+            
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIKey}", APIKey);
+                var json = HttpClient.Downloadstring(url);
+
+                Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json);
+
+                picIcon.ImageLocation = "https://openweathermap.org/img/w/" + myDeserializedClass.Weather[0].icon + ".png";
+
+                String Condition = myDeserializedClass.Weather[0].main;
+                double temp = myDeserializedClass.Weather[0].description;
+                long sunset= myDeserializedClass.Sys.sunset.ToString();
+                long sunrise = myDeserializedClass.Sys.sunrise.ToString();
+                double humidity = myDeserializedClass.Main.humidity.ToString();
+                double pressure = myDeserializedClass.Main.pressure.ToString;
+                double speed = myDeserializedClass.Wind.speed.ToString();
+                
+            }
+        }
+
+    }       
+
+}
+
+
+
+
+
+
+//public static async Task Main()
+  //  {
+    //    var class = new Class1();
+//
+     //   Console.WriteLine("Please enter the coord of a city to get its weather(lat,lon); ");
+  ///      var input = Console.ReadLine();
+       // var coord= Coordinates.BuildFromInput(input);
+        //var weatherObject=await class.GetWeatherAtCoord(coord);
+        //var weather= weather.data.weather.description;
+        //Console.WriteLine($"At coord({lat},{lon}), the weater is : {weather.data.weather.description}");
+
+    //}
+    //catch (Excepyion e){
+        //Console.Writeline(e);
+        //throw;
+    //}
---
+++ lib/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace lib;
+public class Class1
+{
+    public class Coordinates
+    {
+        public float latittude { get; set; }
+        public float longitude { get; set; }
+    }
+
+    public Coordinates (float lat, float lon){
+        latittude = lat;
+        longitude =lon;
+
+
+    }
+    public static Coordinates BuildFromInput(string input){
+        if (string.IsNullOrWhiteSpace(input)) return new Coordinates(39.31f, -74.5f);
+        var result = input.Split(',');
+        float.TryParse(new string(result[0].Where(char.IsDigit).ToArray()), out var lat);
+        float.TryParse(new string(result[1].Where(char.IsDigit).ToArray()), out var lon);
+
+    }
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Main
+    {
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public double pressure { get; set; }
+        public double humidity { get; set; }
+    }
+
+     public class Wind
+    {
+        public double speed { get; set; }
+        public int deg { get; set; }
+    }
+
+    public class Sys
+    {
+        public int type { get; set; }
+        public int id { get; set; }
+        public string country { get; set; }
+        public long sunrise { get; set; }
+        public long sunset { get; set; }
+
+    }
+
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+    public class Root
+    {
+        public Coordinates coord { get; set; }
+        public List<Weather> weather { get; set; }
+        public string @base { get; set; }
+        public Main main { get; set; }
+        public int visibility { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        public int timezone { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
+    }
+}
